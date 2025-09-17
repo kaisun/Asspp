0b8b08dfd188d08cd91160378d70de21a41f9829
Lars <134181853+bo2themax@users.noreply.github.com>
2025-09-17 10:49:48 +0200

    Add Run SwiftFormat for Debug Configuration

--- Asspp/Interface/Welcome/SimpleInstrction.swift
+++ Asspp/Interface/Welcome/SimpleInstrction.swift
@@ -16,37 +16,37 @@ struct SimpleInstrction: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
 
             Divider().hidden()
-            
+
             HStack {
                 Image(systemName: "1.circle.fill")
                 Text("Sign in to your account.")
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             HStack {
                 Image(systemName: "2.circle.fill")
                 Text("Search for apps you want to install.")
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             HStack {
                 Image(systemName: "3.circle.fill")
                 Text("Download and save the ipa file.")
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             HStack {
                 Image(systemName: "4.circle.fill")
                 Text("Install the certificate in settings page.")
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             HStack {
                 Image(systemName: "5.circle.fill")
                 Text("Install or AirDrop to install.")
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             Divider().hidden()
         }
         .font(.system(.body))
