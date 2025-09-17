a42ad608c7611d66722c3351afd5d60764ad3dc1
Lars <134181853+bo2themax@users.noreply.github.com>
2025-09-17 11:12:35 +0200

    Use exact version of SwiftFormat

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "BuildTools",
     platforms: [.macOS(.v11)],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.57.2"),
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
